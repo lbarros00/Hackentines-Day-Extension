d736d0b233b7e650ff280a2b3d7c521d0a2a2e30
lbarros00 <laisa.barros26@gmail.com>
2017-02-11 15:52:14 -0500

    first commit adding files
    Simple implementation with Google Fonts and Paint Image.

---
+++ popup.js
@@ -0,0 +1,15 @@
+var upliftMeQuotes = [
+	"&quot;You are cute, can I keep you?&quot;",
+	"&quot;I love you from my head tomatoes&quot;",
+	"&quot;I like when you are smile, but I love it when I&#39;m the reason.&quot;",
+	"&quot;Good friends are like stars,you don&#39;t always see them, but you know they are always there.&quot;",
+	"&quot;Dear, whoever read this. I hope you have the reason to smile today.&quot;",
+	"&quot;Life is short... smile when you are still have teeth.&quot;",
+	"&quot;Will you be my valentine?&quot;",
+	"&quot;I love my computer because my friends live in it!&quot;",
+	"&quot;I think it&#39;s time to be happy again.&quot;"
+];
+
+var randomNum = Math.floor(Math.random() * 8);
+
+document.write(upliftMeQuotes[randomNum]);
